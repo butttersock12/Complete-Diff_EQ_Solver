7089089addfbeddb0e8ee1ca5078ac97b62b4991
butttersock12 <127546492+butttersock12@users.noreply.github.com>
2023-03-10 19:44:18 -0500

    Update Testing.py

--- Testing.py
+++ Testing.py
@@ -5,84 +5,46 @@ import plotly.graph_objects as go
 import numpy as np
 import sympy as sp
 import sys
+from flask import Flask
 
-def differential_equation_calculator_title():
-    print("""
-#########################################################################################    
-██████╗ ██╗███████╗███████╗███████╗██████╗ ███████╗███╗   ██╗████████╗██╗ █████╗ ██╗     
-██╔══██╗██║██╔════╝██╔════╝██╔════╝██╔══██╗██╔════╝████╗  ██║╚══██╔══╝██║██╔══██╗██║     
-██║  ██║██║█████╗  █████╗  █████╗  ██████╔╝█████╗  ██╔██╗ ██║   ██║   ██║███████║██║     
-██║  ██║██║██╔══╝  ██╔══╝  ██╔══╝  ██╔══██╗██╔══╝  ██║╚██╗██║   ██║   ██║██╔══██║██║     
-██████╔╝██║██║     ██║     ███████╗██║  ██║███████╗██║ ╚████║   ██║   ██║██║  ██║███████╗
-╚═════╝ ╚═╝╚═╝     ╚═╝     ╚══════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═══╝   ╚═╝   ╚═╝╚═╝  ╚═╝╚══════╝
-                                                                                         
-        ███████╗ ██████╗ ██╗   ██╗ █████╗ ████████╗██╗ ██████╗ ███╗   ██╗                
-        ██╔════╝██╔═══██╗██║   ██║██╔══██╗╚══██╔══╝██║██╔═══██╗████╗  ██║                
-        █████╗  ██║   ██║██║   ██║███████║   ██║   ██║██║   ██║██╔██╗ ██║                
-        ██╔══╝  ██║▄▄ ██║██║   ██║██╔══██║   ██║   ██║██║   ██║██║╚██╗██║                
-        ███████╗╚██████╔╝╚██████╔╝██║  ██║   ██║   ██║╚██████╔╝██║ ╚████║                
-        ╚══════╝ ╚══▀▀═╝  ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝                
-                                                                                         
-                ███████╗ ██████╗ ██╗    ██╗   ██╗███████╗██████╗                         
-                ██╔════╝██╔═══██╗██║    ██║   ██║██╔════╝██╔══██╗                        
-                ███████╗██║   ██║██║    ██║   ██║█████╗  ██████╔╝                        
-                ╚════██║██║   ██║██║    ╚██╗ ██╔╝██╔══╝  ██╔══██╗                        
-                ███████║╚██████╔╝███████╗╚████╔╝ ███████╗██║  ██║                        
-                ╚══════╝ ╚═════╝ ╚══════╝ ╚═══╝  ╚══════╝╚═╝  ╚═╝                        
-#########################################################################################""")
-    return
-def second_order_solver_title():
-    print(""" 
- ___         _   ___         _             ___       _                
-<_  >._ _  _| | | . | _ _  _| | ___  _ _  / __> ___ | | _ _  ___  _ _ 
- / / | ' |/ . | | | || '_>/ . |/ ._>| '_> \__ \/ . \| || | |/ ._>| '_>
-<___>|_|_|\___| `___'|_|  \___|\___.|_|   <___/\___/|_||__/ \___.|_|  
-                                                                     """)
-    return
-def second_order_solver():
-    os.system('cls')
-    second_order_solver_title()
-    print("Avaliable Operations:")
-    print(" ")
-    print("- Conventional -")
-    print("(1) Homogenoues Solution Calculator")
-    print("(2) Complete/Particular Solution Calculator")
-    print("(3) Root Calculator")
-    print("(4) Guess Table")
-    print("(5) Graphing")
-    print(" ")
-    print("- Advanced - ")
-    print("(6) Measurement System")
-    print("(7) Basic Parallel RLC Circuit")
-def unsupported_input():
-    print("""
-                Unsupported Input...""")
-    print("Availbale options:")
-    print("(1) Main Menu")
-    print("(2) Exit")
-    unsupported_input = input("")
-    if unsupported_input == "1":
-        os.system('cls')
-    else:
-        os.system('cls')
-        raise SystemExit
+app = Flask(__name__)
+
+@app.route("/")
 def start_up():
-    print("""  
+    output = """
                        Where would you like to go?
-    """)
-    
-    print("                    1 --> 1st Order Differential Solver")
-    print("                    2 --> 2nd Order Differential Solver")
-    print("""                    3 --> Laplace Transform Solver
-    """)
-    main_selection = input("                      Choose: ")
-    if main_selection == "2":
-        second_order_solver()
-    else: 
-        unsupported_input() 
 
-os.system('clc')
-differential_equation_calculator_title()
-time.sleep(1)
-time.sleep(1)
-start_up()
+                        1 --> 1st Order Differential Solver
+                        2 --> 2nd Order Differential Solver
+                        3 --> Laplace Transform Solver
+    """
+    return output
+
+@app.route("/second-order-solver")
+def second_order_solver():
+    output = """
+    <h3>Available Operations:</h3>
+    <ul>
+        <li>Homogenoues Solution Calculator</li>
+        <li>Complete/Particular Solution Calculator</li>
+        <li>Root Calculator</li>
+        <li>Guess Table</li>
+        <li>Graphing</li>
+        <li>Measurement System</li>
+        <li>Basic Parallel RLC Circuit</li>
+    </ul>
+    """
+    return output
+
+@app.route("/unsupported-input")
+def unsupported_input():
+    output = """
+                Unsupported Input...
+                Available options:
+                1) Main Menu
+                2) Exit
+    """
+    return output
+
+if __name__ == "__main__":
+    app.run()
